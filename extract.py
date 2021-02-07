1a6fc4f4c2dc1fac03389e75e623777534a14c0e
stardark2 <dem1116@naver.com>
2021-02-07 14:19:52 +0000

    max page는 잘나옴 요청에서 오류

--- extract.py
+++ extract.py
@@ -1,28 +1,36 @@
 import requests
 from bs4 import BeautifulSoup
 LIMIT = 1
-URL =   f"https://www.menupan.com/restaurant/bestrest/bestrest"
-def extract_hotplace():
-  hotp_result = requests.get(URL)
-  hotp_soup = BeautifulSoup(hotp_result.text, "html.parser")
+ranking = "rt" 
+URL = f"https://www.menupan.com/restaurant/bestrest/bestrest.asp"
 
-  paging = hotp_soup.find("div", class_="paging")
-  first = paging.find("span",class_="pgClick").get_text()
-  pagination = [first]
-  pages = paging.find_all("a") 
-  pages = pages[2:-2]
-  for page in pages:
-    pagination.append(page.string)
-  max_page = pagination[-1]
-  return max_page
+
+def extract_hotplace():
+    hotp_result = requests.get(URL)
+    hotp_soup = BeautifulSoup(hotp_result.text, "html.parser")
+
+    paging = hotp_soup.find("div", class_="paging")
+    first = paging.find("span", class_="pgClick").get_text()
+    pagination = [first]
+    pagination = []
+    pages = paging.find_all("a")
+    pages = pages[2:-2]
+    for page in pages:
+        pagination.append(page.string)
+    max_page = pagination[-1]
+    return max_page
+
 
 def extract_hotp_lope(last_page):
-  jobs = []
-  for page in range(last_page):
-    result = requests.get(f"{URL}.asp?page={LIMIT*page}&trec=9817&pt=rt")
-    soup = BeautifulSoup(result.text, "html.parser")
-    results = soup.find_all("li")
-    for result in results:
-      title = result.find("p", class_="listName").find("span",class_="restName").find("a")
-      food_type = result.find("p", class_="listType")
-  return jobs
+    restaurant = []
+    for page in range(last_page):
+        call = requests.get(f"{URL}?page={LIMIT*page}&trec=9774&pt={ranking}")
+        soup = BeautifulSoup(call.text, "html.parser")
+        results = soup.find_all("li")
+        for result in results:
+            title = result.find("p", class_="listName").find(
+                "span", class_="restName").find("a")
+            food_type = result.find("p", class_="listType")
+            area = result.find("p", class_="listArea")
+            restaurant.append(title, food_type, area)
+    return restaurant
--- page.py
+++ page.py
@@ -1,3 +1,3 @@
 from extract import extract_hotp_lope, extract_hotplace
 
-  
+print(extract_hotplace())
